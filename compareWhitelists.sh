00ea9d80e6edc9f3f91a577d5920c4428d24c719
Manveer Chauhan <mschauhan@spartan-login1.hpc.unimelb.edu.au>
2023-06-20 11:26:27 +1000

    created separate txt and csv output files

---
+++ compareWhitelists.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Script that compares the barcodes listed in two whitelist.csv files, to run the script:
+# 1) save it to a file (e.g., compareWhitelists.sh)
+# 2) make it executable using chmod +x compareWhitelists.sh
+# 3) execute it using ./compareWhitelists.sh [~/file1.csv] [~/file2.csv] in the terminal
+# The script will generate the non_matching_barcodes.txt file containing the desired output
+
+file1="$1"
+file2="$2"
+
+# Assign the name of the txt containing the non matching barcode count for each file
+# and the csv file listing the actual non matching barcodes with their file identifier
+txtFileOutput="Non Matching Barcodes Counts.txt"
+csvFileOutput="List of non-matching barcodes.csv"
+
+# Write the header of the csv file
+echo "Barcode,File of Origin" > "$csvFileOutput"
+
+# Check if both file paths are provided
+if [ -z "$file1" ] || [ -z "$file2" ]; then
+  echo "Please provide two BLAZE whitelist.csv file paths as parameters."
+  exit 1
+fi
+
+# Get the number of lines in each file
+file1_lines=$(wc -l < "$file1")
+file2_lines=$(wc -l < "$file2")
+
+# Compare the lines of the two CSV files and write non-matching lines to the output file
+comm_output=$(comm -23 <(sort "$file1") <(sort "$file2"))
+non_matching_count_file1=$(comm -23 <(sort "$file1") <(sort "$file2") | wc -l)
+non_matching_count_file2=$(comm -13 <(sort "$file1") <(sort "$file2") | wc -l)
+
+# Write the file paths, line counts, and non-matching line counts to the output file
+echo "File 1 ($file1_lines barcodes): $file1" > "$txtFileOutput"
+echo "File 2 ($file2_lines barcodes): $file2" >> "$txtFileOutput"
+echo "Non-matching barcodes found in File 1: $non_matching_count_file1" >> "$txtFileOutput"
+echo "Non-matching barcodes found in File 2: $non_matching_count_file2" >> "$txtFileOutput"
+
+echo
+echo "Non-matching barcode counts for each file + file identifiers are saved in $txtFileOutput"
+
+# Append the non-matching lines with the corresponding file identifier to the csv output file:
+while read -r line; do
+  echo "$line,File 1" >> "$csvFileOutput"
+done < <(comm -23 <(sort "$file1") <(sort "$file2"))
+
+while read -r line; do
+  echo "$line,File 2" >> "$csvFileOutput"
+done < <(comm -13 <(sort "$file1") <(sort "$file2"))
+
+echo "List of non-matching barcodes are saved in $csvFileOutput"
+echo
