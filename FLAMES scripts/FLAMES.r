11b0b5d1a7811d101ceb084d015712c8f9bd9f3f
Manveer Chauhan <mschauhan@spartan-login1.hpc.unimelb.edu.au>
2023-06-22 07:24:31 +1000

    Fixed syntax and changed gtf file

--- FLAMES scripts/FLAMES.r
+++ FLAMES scripts/FLAMES.r
@@ -16,7 +16,6 @@ main <- function() {
 
     # Define the variables that are unique to the sample-------------------------
     args <- commandArgs(trailingOnly = TRUE)
-
     # Check that the number of arguments parsed into the function are correct
     if (length(args) != 3) {
         stop("Please provide the correct number of arguments.
@@ -28,13 +27,15 @@ main <- function() {
     output <- args[3]
 
 
-    # Define global variables (these shouldn't change between samples)-----------
+    # DEFINE GLOBAL VARIABLES HERE (these shouldn't change between samples)-----------
     minimap2_path <- "~/.conda/envs/minimap2/bin/minimap2"
     # Define reference files
-    GTF <- "/data/gpfs/projects/punim0646/manveer/gencode.v43.chr_patch_hapl_scaff.annotation.gtf.gz"
+    GTF <- "/data/gpfs/projects/punim0646/manveer/gencode.v43.basic.annotation.gtf.gz"
     transcriptome <- "/data/gpfs/projects/punim0646/manveer/gencode.v43.transcripts.fa"
     # Enter the path for the config file
-        configPath <- "/data/gpfs/projects/punim0646/manveer/config_file_57972.json"
+    configPath <- "/data/gpfs/projects/punim0646/manveer/config_file_57972.json"
+
+
 
     # Run FLAMES-----------------------------------------------------------------
     sce <- sc_long_pipeline(fastq = fastqfile, outdir = output, reference_csv = whitelist, annotation = GTF, 
