69fe11e2ab5495d73f8e9b05fcbc009d8ccdfe9e
Manveer Chauhan <mschauhan@spartan-login1.hpc.unimelb.edu.au>
2023-06-28 16:12:31 +1000

    Used the gridExtra package to generate a plots pdf

--- countMatrix_to_UMAP.R
+++ countMatrix_to_UMAP.R
@@ -1,12 +1,15 @@
 # SCRIPT THAT GENERATES UMAP (and other) PLOTS FOR BLAZE+FLAMES GENERATED COUNT MATRICES
 
 # Load libraries
-#BiocManager::install("Seurat")
-#reticulate::py_install(packages = 'umap-learn')
-#install.packages("tidyverse")
 library(Seurat)
 library(tidyverse)
-install.packages("gridExtra")
+library(ggplot2)
+library(gridExtra)
+library(grid)
+
+# Create a list to store the plots that we generate
+global_figures = list()
+additional_figures = list()
 
 ### STEP 0 : CREATING SEURAT OBJECT----------------------------------------------
 ## Read in the transcript count csv file
@@ -70,6 +73,7 @@ print(seurat.obj[["pca"]], dims = 1:5, nfeatures = 5)
 
 # Determine the dimensionality of our data
 ElbowPlot(seurat.obj) # It seems that 15 PCs are sufficient to explain the variation within our data
+global_figures[[1]] <- ElbowPlot(seurat.obj)
 
 ### STEP 7 : CLUSTERING ---------------------------------------------------------
 seurat.obj <- FindNeighbors(seurat.obj, dims = 1:10)
@@ -85,46 +89,70 @@ DimPlot(seurat.obj, group.by = "RNA_snn_res.0.7", label = TRUE)
 DimPlot(seurat.obj, group.by = "RNA_snn_res.1", label = TRUE)
 
 # With a resolution of 0.7, cells in each clusters seem distinctly separated
-# thus 0.7 seems appropriate for this Q20 dataset
+# thus 0.7 seems appropriate for this Q20 dataset. ****ADD 0.7 PLOT to the list*****
 
 # setting the resolution for cell cluster identities
 Idents(seurat.obj) <- "RNA_snn_res.0.7"
 Idents(seurat.obj)
 
-## STEP 8 : NON-LINEAR DIMENSIONALITY REDUCTION --------------------------------
+### STEP 8 : NON-LINEAR DIMENSIONALITY REDUCTION --------------------------------
 seurat.obj <- RunUMAP(seurat.obj, dims = 1:10)
 baseUMAPplot <- DimPlot(seurat.obj, reduction = "umap", label = TRUE, ) + 
   labs(color = "cluster \n(from PCA)", 
-       title = 'Q20 Day 25 cort-diff Sample \n(Resolution: 0.7, Dimensions: 10')
+       title = 'Q20 Day 25 cort-diff Sample \n(Resolution: 0.7, Dimensions: 10') +
+  ggmin::theme_powerpoint()
+#baseUMAPplot
+global_figures[[2]] <- baseUMAPplot
 
-#Install an alternative (cleaner) Seurat-applied theme called ggmin
-#install.packages("devtools")  
-#devtools::install_github("sjessa/ggmin")
-baseplot + ggmin::theme_powerpoint()
-
-## STEP 9 : FIND CLUSTER MARKERS------------------------------------------------
+### STEP 9 : FIND CLUSTER MARKERS------------------------------------------------
 q20_cluster_markers <- FindAllMarkers(seurat.obj, only.pos = TRUE,
                              min.pct = 0.25, logfc.threshold = 0.25)
 q20_cluster_markers
 
-## STEP 10 : GENERATE TRANSCRIPT EXPRESSION UMAPS-------------------------------
+### STEP 10 : GENERATE TRANSCRIPT EXPRESSION UMAPS-------------------------------
+
+## Create a UMAP plot showing the number of RNA molecules per cell
 UMIcountUMAP <- FeaturePlot(seurat.obj, reduction = "umap", features = 'nCount_RNA')+
-  labs(color = "UMI count",title = '')+ theme(text = element_text(size = 10))
-UMIcountUMAP + ggmin::theme_powerpoint()
+  labs(color = "UMI count",title = '')+ theme(text = element_text(size = 10)) + ggmin::theme_powerpoint()
+#UMIcountUMAP
+global_figures[[3]] <- UMIcountUMAP
+#global_figures[[3]]
 
+
+## Create a UMAP plot showing the number of features expressed per cell
 FeatureCountUMAP <- FeaturePlot(seurat.obj, reduction = "umap", features = 'nFeature_RNA')+
-  labs(color = str_wrap("Isoform count",15),title = '')+ theme(text = element_text(size = 10))
-FeatureCountUMAP + ggmin::theme_powerpoint()
+  labs(color = str_wrap("Isoform count",15),title = '')+ theme(text = element_text(size = 10)) +
+  ggmin::theme_powerpoint()
+#FeatureCountUMAP 
+global_figures[[4]] <- FeatureCountUMAP
 
+## Create a UMAP plot showing the expression levels for specified isoforms of interest
 top10
+# Here I chose three different isoforms from the top10 most variable isoform list (commented above)
 isoforms_of_interest = c("ENSG00000104435.14-79611117-79665011-1", 
                          "ENST00000314744.6", 
-                         "ENSG00000131711.15-72107336-72195422-1")
-isoforms_of_interest_figures <- list()
+                         "ENSG00000131711.15-72107336-72195422-1",
+                         "ENSG00000149591.17-117199370-117204778-1")
+
+# Genes that correspond to isoforms listed above : 
+# STMN2 [2], NEUROG1 [8], MAP1B [9], TAGLN [1]
+IoI_figures <- list()
 for (isoform in isoforms_of_interest) {
-  isoforms_of_interest_figures <- append(rst_figures, 
-                                         FeaturePlot(seurat.obj, reduction = 'umap',
-                                                                 features = isoform,
-                                                                 order = TRUE) + ggmin::theme_powerpoint())
+  IoI_figures[[isoform]] <- FeaturePlot(seurat.obj, reduction = 'umap', 
+                                        features = isoform,
+                                        order = TRUE) + ggmin::theme_min()
 }
-grid.
+
+# Create the grid layout to view expression levels of the Isoforms of interest
+grid_layout_IoI <- grid.arrange(grobs = IoI_figures, ncol = 2, 
+                                top=textGrob("Isoform Expression levels across Cells \n(Taken from Top 10 most variable features)\n", 
+                                             gp = gpar(fontsize = 12, fontface = "bold")))
+
+             
+### STEP 11 : COMPILE PLOTS IN GRID FORMAT--------------------------------------
+
+## Create a grid layout for the global_figures list
+grid_layout_main <- grid.arrange(grobs = global_figures, ncol = 2)
+
+## Concatenate layout with isoforms of interest plots
+concatenated_layout <- grid.arrange(grid_layout_IoI, grid_layout_main)
