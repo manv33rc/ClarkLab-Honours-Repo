cc5b170c4e31170bbabab24a32ef6ac6aa09d3fc
manveerchauhan <manv33rc@gmail.com>
2023-07-04 10:45:23 +1000

    updated figure labels for pipeline script 1

--- countMatrix-analysis-pipeline/Script-1_preliminary_filtering.R
+++ countMatrix-analysis-pipeline/Script-1_preliminary_filtering.R
@@ -29,13 +29,13 @@ df <- as.data.frame(transcript.percent.expression)
 # Create a boxplot to visualize the distribution of transcript expression percentages across cells for each gene
 before.filter.box <- ggplot(df, aes(y = "", x = transcript.percent.expression)) +
   geom_boxplot() +
-  labs(y = "", x = "Expression Percentage (%)", title = "BEFORE removing isoforms \nexpressed in =<1% of cells") +
+  labs(y = "", x = "Isoform expression percentage (%)", title = "BEFORE removing isoforms \nexpressed in =<1% of cells") +
   theme_classic() +
   scale_x_continuous(breaks = seq(0, 100, by = 10)) +
   scale_y_discrete(breaks = NULL)
 #Generate a table of 5 point summary stastics for transcript expression percentages across cells for each gene
 summary_table.before <- as.data.frame(summary(df)) %>% subset(., select=Freq) 
-colnames(summary_table.before)[1] <- "Expression Frequency \nBEFORE Filtering (%)"
+colnames(summary_table.before)[1] <- "Isoform expression frequency \nacross cells (%) \nBEFORE Filtering (%)"
 
 
 ## Filter out transcripts that are expressed in <=1% of all cells (to avoid potential confounding false positives from FLAMES)----
@@ -44,10 +44,10 @@ count_matrix.subset <- count_matrix[transcript.filter,] # create subset of our c
 num_removed_trans = length(names(transcript.percent.expression)) - length(transcript.filter)
 
 table1 <- data.frame(
-  "Number of Transcript Removed" = num_removed_trans,
-  "Total Counts Before Filtering" = sum(count_matrix),
-  "Total Counts After Filtering" = sum(count_matrix.subset),
-  "Number of Raw Counts Removed" = sum(count_matrix) - sum(count_matrix.subset)
+  "Number of Isoforms Removed" = num_removed_trans,
+  "Total Reads Before Filtering" = sum(count_matrix),
+  "Total Reads After Filtering" = sum(count_matrix.subset),
+  "Number of Raw Read Counts Removed" = sum(count_matrix) - sum(count_matrix.subset)
 ) %>% 
   t(.)
 
@@ -64,7 +64,7 @@ after.filter.box <- ggplot(df2, aes(y = "", x = filtered.trans.percent.expressio
 # Generate a table of 5 point summary stastics for transcript expression percentages 
 # across cells for each gene
 summary_table.after <- as.data.frame(summary(df2)) %>% subset(., select=Freq) 
-colnames(summary_table.after)[1] <- "Expression Frequency \nAFTER Filtering (%)"
+colnames(summary_table.after)[1] <- "Isoform expression frequency \nAFTER filtering (%)"
 
 summary_table.concat <- cbind(summary_table.before, summary_table.after)
 
@@ -84,14 +84,16 @@ vlnplot1 <- VlnPlot(seurat.obj, features = c("nCount_RNA")) + labs(title = "Tota
 vlnplot2 <- VlnPlot(seurat.obj, features = c("nFeature_RNA")) + labs(title = "Total unique isoforms\nper cell BEFORE filtering") + 
   theme(legend.position = "none")
 association.plot1 <- FeatureScatter(seurat.obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") +
-  geom_smooth(method = "lm") + labs(title = "Correlation plot of RNA molecules and distinct transcripts per cell BEFORE filtering")
+  geom_smooth(method = "lm") + labs(title = "Correlation plot of RNA molecules and distinct transcripts per cell BEFORE filtering") + 
+  theme(legend.position = "none")
 
 vlnplot3 <- VlnPlot(seurat.obj.filtered, features = c("nCount_RNA")) + labs(title = "Total RNA molecules (reads)\nper cell AFTER filtering") + 
   theme(legend.position = "none")
 vlnplot4 <- VlnPlot(seurat.obj.filtered, features = c("nFeature_RNA")) + labs(title = "Total unique isoforms\nper cell AFTER filtering") + 
   theme(legend.position = "none")
 association.plot2 <- FeatureScatter(seurat.obj.filtered, feature1 = "nCount_RNA", feature2 = "nFeature_RNA") +
-  geom_smooth(method = "lm") + labs(title = "Correlation plot of RNA molecules and distinct transcripts per cell AFTER filtering")
+  geom_smooth(method = "lm") + labs(title = "Correlation plot of RNA molecules and distinct transcripts per cell AFTER filtering") + 
+  theme(legend.position = "none")
 
 # Violin plot comparisons
 vlnplot_comparisons <- grid.arrange(vlnplot1, vlnplot3, vlnplot2, vlnplot4, nrow = 2)
@@ -105,7 +107,7 @@ write.csv(count_matrix.subset, filename)
 concatenated.layout <- grid.arrange(summary_statistics.figs, vlnplot_comparisons, association_comparisions,
                                     top=textGrob("CHECK THAT DISTRIBUTION OF READS AND ISOFORMS\nARE GOOD AFTER FILTERING FEATURES\n(to reduce false positives from FLAMES isoform quantification)\n"))
 pdfFilename <- paste(output_prefix, "-script1.pdf", sep="")
-ggsave(pdfFilename, concatenated.layout, width = 10, height = 25)
+ggsave(pdfFilename, concatenated.layout, width = 11, height = 25)
 
 # if all cells seem to be good quality based on the preliminary feature filter -
 # move on to more QC in next script
