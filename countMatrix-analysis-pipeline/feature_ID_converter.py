7fbcef02959341b5bbec090c72954610a258b599
manveerchauhan <manv33rc@gmail.com>
2023-08-08 11:01:24 +1000

    Added functions to convert geneID/name lists

--- countMatrix-analysis-pipeline/feature_ID_converter.py
+++ countMatrix-analysis-pipeline/feature_ID_converter.py
@@ -94,7 +94,7 @@ def CheckNovelIsoform(feature_id, ConvType, gtfFilePath, novelIsoTitle):
         return 'No novel isoform detected'
         
 # Main function that retrieve the desired corresponding feature name to the provided gene/transcript ID
-def FeatureIDtoSymbol(feature_id, ConvType, gtfFilePath, novelIsoTitle = False):
+def FeatureIDtoName(feature_id, ConvType, gtfFilePath, novelIsoTitle = False):
     # Check if conversion type parameter is in correct format
     valid_ConversionTypes = ['t.to.tName', 'g.to.gName', 't.to.gName']
     if ConvType not in valid_ConversionTypes:
@@ -125,33 +125,83 @@ def FeatureIDtoSymbol(feature_id, ConvType, gtfFilePath, novelIsoTitle = False):
             print(f"No matching names found for the provided feature ID: {feature_id}")
             return feature_id
 
+# Function to get a list of gene names for a given list of gene IDs
+def ListofGeneIDstoNames(gene_ids, gtfFilePath):
+    gene_names = []  # Initialize the list to store gene names
+
+    # Build a hashmap of gene ID to gene name key-value pairs
+    feature_map = build_feature_map('g.to.gName', gtfFilePath)
+
+    for gene_id in gene_ids:
+        # Remove the "-PAR-Y" suffix using regular expression
+        gene_id_without_suffix = re.sub(r"-PAR-Y$", "", gene_id)
+        
+        # Retrieve the gene name from the hashmap based on gene ID
+        gene_name = feature_map.get(gene_id_without_suffix)
+        if gene_name:
+            gene_names.append(gene_name)
+        else:
+            print(f"Couldn't find gene name for {gene_id}, appending None")
+            gene_names.append(None)  # Append None if no matching gene name is found
+
+    return gene_names
+
+# Function to get a list of gene IDs for a given list of gene names
+def ListofGeneNamestoIDs(gene_names, gtfFilePath):
+    gene_ids = []  # Initialize the list to store gene IDs
+
+    # Build a hashmap of gene name to gene ID key-value pairs
+    feature_map = build_feature_map('g.to.gName', gtfFilePath)
+
+    for gene_name in gene_names:
+        gene_id = None
+        # Search the feature_map for the gene ID that matches the provided gene name
+        for feature_id, feature_name in feature_map.items():
+            if feature_name == gene_name:
+                gene_id = feature_id
+                break
+
+        if gene_id:
+            gene_ids.append(gene_id)
+        else:
+            print(f"Couldn't find gene ID for gene name: {gene_name}, appending None")
+            gene_ids.append(None)  # Append None if no matching gene ID is found
+
+    return gene_ids
+
 def TestScript():
     gtf_file = '/data/gpfs/projects/punim0646/manveer/gencode.v43.chr_patch_hapl_scaff.annotation.gtf'
-    gene_id = 'ENSG00000104435.14'   # Gene Name: DDX11L1
+    gene_id = 'ENSG00000104435.14'   # Gene Name: STMN2
+    gene_id2 = 'ENSG00000237613.2'   # Gene Name: FAM138A
+    gene_list = [gene_id, gene_id2]
     transcript_id = 'ENSG00000104435.14-79611117-79665011-1'
     transcript_id2 = 'ENST00000641515.2' # transcript name : OR4F5-201
     
-    print('\n')
+    geneNameList = ['STMN2', 'FAM138A']
+    gtf_file = '/data/gpfs/projects/punim0646/manveer/gencode.v43.chr_patch_hapl_scaff.annotation.gtf'
+    print(ListofGeneNamestoIDs(geneNameList, gtf_file))
+    
+    print("TESTING GENE ID LIST TO NAMES FUNCTION")
+    print(ListofGeneIDstoNames(gene_list, gtf_file))
+    print('\n\n')
     # Retrieve desired name based on feature symbol and conversion type
-    print(f"return value: {FeatureIDtoSymbol(gene_id, 'g.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: STMN2\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id, 'g.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: novel trans ID\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id, 't.to.tName', gtf_file, novelIsoTitle=True)} \nExpected output: novel trans ID title\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id, 't.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: STMN2\n")
+    print(f"return value: {FeatureIDtoName(gene_id, 'g.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: STMN2\n")
+    print(f"return value: {FeatureIDtoName(transcript_id, 'g.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: novel trans ID\n")
+    print(f"return value: {FeatureIDtoName(transcript_id, 't.to.tName', gtf_file, novelIsoTitle=True)} \nExpected output: novel trans ID title\n")
+    print(f"return value: {FeatureIDtoName(transcript_id, 't.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: STMN2\n")
 
     print("Transcript 2 : OR4F5-201--------")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id2, 'g.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: transcript2 ID\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id2, 't.to.tName', gtf_file, novelIsoTitle=True)} \nExpected output: OR4F5-201\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id2, 't.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: OR4F5\n")
+    print(f"return value: {FeatureIDtoName(transcript_id2, 'g.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: transcript2 ID\n")
+    print(f"return value: {FeatureIDtoName(transcript_id2, 't.to.tName', gtf_file, novelIsoTitle=True)} \nExpected output: OR4F5-201\n")
+    print(f"return value: {FeatureIDtoName(transcript_id2, 't.to.gName', gtf_file, novelIsoTitle=True)} \nExpected output: OR4F5\n")
 
     print('\nnovelIsoTitle = False')
-    print(f"return value: {FeatureIDtoSymbol(gene_id, 'g.to.gName', gtf_file)} \nExpected output: STMN2\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id, 'g.to.gName', gtf_file)} \nExpected output: Novel trans ID\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id, 't.to.tName', gtf_file)} \nExpected output: Novel trans ID\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id, 't.to.gName', gtf_file)} \nExpected output: STMN2\n")
+    print(f"return value: {FeatureIDtoName(gene_id, 'g.to.gName', gtf_file)} \nExpected output: STMN2\n")
+    print(f"return value: {FeatureIDtoName(transcript_id, 'g.to.gName', gtf_file)} \nExpected output: Novel trans ID\n")
+    print(f"return value: {FeatureIDtoName(transcript_id, 't.to.tName', gtf_file)} \nExpected output: Novel trans ID\n")
+    print(f"return value: {FeatureIDtoName(transcript_id, 't.to.gName', gtf_file)} \nExpected output: STMN2\n")
 
     print("Transcript 2 : OR4F5-201--------")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id2, 'g.to.gName', gtf_file)} \nExpected output: transcript2 ID\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id2, 't.to.tName', gtf_file)} \nExpected output: OR4F5-201\n")
-    print(f"return value: {FeatureIDtoSymbol(transcript_id2, 't.to.gName', gtf_file)} \nExpected output: OR4F5\n")
-
-TestScript()
+    print(f"return value: {FeatureIDtoName(transcript_id2, 'g.to.gName', gtf_file)} \nExpected output: transcript2 ID\n")
+    print(f"return value: {FeatureIDtoName(transcript_id2, 't.to.tName', gtf_file)} \nExpected output: OR4F5-201\n")
+    print(f"return value: {FeatureIDtoName(transcript_id2, 't.to.gName', gtf_file)} \nExpected output: OR4F5\n")
